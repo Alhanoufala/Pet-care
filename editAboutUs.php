8f741c66fd5b587dcdcd39bcfc5c820c5b76da0c
Raseel-alrowais <rasseel.001@gmail.com>
2022-05-04 22:01:38 +0300

    .

--- editAboutUs.php
+++ editAboutUs.php
@@ -14,10 +14,10 @@
         <div class="navbar"></div>
         <div class="container">
             <h2>Edit About us </h2>
-            <form method="post" >
+            <form method="post" action = "editAboutUs.php">
                 Label:
                 
-                <input type="text" id="service name" value="About us"> 
+                <input type="text" name ="label" value="About us"> 
 
                 Description:
                 <textarea name = "Description" id="Description"rows="20" cols = "66">Welcome to our Petcare Veterinary Clinic! Where animals and their owners are valued and cherished. 
@@ -29,7 +29,7 @@
                     <input type="file" id="PhotoFile" name="PhotoFile">
                     Location :
                 
-                <input type="text" id="service name" value="location"> 
+                <input type="text" name ="location" value="location"> 
 
 
     
@@ -41,7 +41,28 @@
                 
             </form>
             
-             
+            <?php
+             if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                 if ( !( $database = mysqli_connect( "localhost", "root", "" ) ) )
+                    die( "<p>Could not connect to database</p>" );
+
+                 if ( !mysqli_select_db( $database, "Pet_care") )
+                    die( "<p>Could not open URL database</p>" );
+
+                    $label =  $_POST["label"];
+                    $description =   $_POST["Description"];
+                    $photo =  $_POST["PhotoFile"];
+                    $location =$_POST["location"];
+
+                 $query=" UPDATE aboutus SET ( Label = '.$label ' , Description = '.$description ' , photo = '.$photo ' , location = '.$location ' ) ;" ;
+                 $result = mysqli_query($database, $query);
+                 if($result)
+                        header("location: ManagerAboutUs.php");
+            
+                    else
+                        echo "An error occured while completing your request.";
+             }
+        ?>
        
      
         </div>
