28924ffe4202fbe32d80e7ef2797ce5345ca60df
Raseel-alrowais <rasseel.001@gmail.com>
2022-05-20 15:19:52 +0300

    appointment details footer

--- appointmentDetails.php
+++ appointmentDetails.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="styles/appointmentDetails.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="styles/appointmentDetails.css?<?=filemtime("styles/appointmentDetails.css")?>" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="styles/footer.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
    
     <title>Edit Appointment Request</title>
