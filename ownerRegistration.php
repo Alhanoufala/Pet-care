3d1fc9591979a48f3b0a19cae4140b877fba827c
SarahIbra <sarahalshathri70@gmail.com>
2022-05-21 03:29:50 +0300

    .

--- ownerRegistration.php
+++ ownerRegistration.php
@@ -21,7 +21,8 @@
             <input type="text" name="Lname" id="Lname" placeholder="Last name" required>
             <input type="text" name="email" id="email" placeholder="Email" required>
             <input type="text" name="phonenumber" id="phonenumber" placeholder="Phone number" required>
-            <input type="password" name="pass" id="pass" placeholder="Password" required>
+            <input type="password" name="pass" id="pass" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at 
+            least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
              
             <div class="content">
               <div class="radio">
